0ffad89c181ebc61e7d27361e482be48f7450446
kamal kishor <ikkamalkish0r@gmail.com>
2023-04-25 23:37:29 +0530

    created play song page, fixed issues with urls and how songs are listed

--- music_share/models.py
+++ music_share/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
-
+from PIL import Image
 
 class MusicFile(models.Model):
     ALLOWED_FILE_TYPES = ('mp3', 'wav', 'flac')
@@ -17,6 +17,7 @@ class MusicFile(models.Model):
 
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     file = models.FileField(upload_to='media/')
+    image = models.ImageField(default='default.jpeg', upload_to='album_art')
     visibility = models.CharField(max_length=10, choices=VISIBILITY_CHOICES, default=PUBLIC)
     title = models.CharField(max_length=150, default="Unknown")
     artist = models.CharField(max_length=50, default="Unknown")
@@ -25,4 +26,16 @@ class MusicFile(models.Model):
 
     def __str__(self) -> str:
         return self.title, self.artist, self.album, self.visibility
+    
+
+    def save(self, *args, **kwargs):
+        kwargs['force_insert'] = False
+        super(MusicFile, self).save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 400 or img.width > 300:
+            output_size = (400, 300)
+            img.thumbnail(output_size, Image.Resampling.LANCZOS)
+            img.save(self.image.path)
 
--- music_share/views.py
+++ music_share/views.py
@@ -1,32 +1,17 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import MusicFile
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from .forms import MusicFileForm
 from django.contrib import messages
 import eyed3
+from django.http import FileResponse
 import os
+from django.http import HttpResponse, Http404
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
 from django.conf import settings
-
-@login_required
-def home(request):
-    all_songs = MusicFile.objects.filter(Q(owner=request.user) | Q(visibility='public') | Q(allowed_emails__contains=[request.user.email]))
-    context = {
-        'songs' : all_songs
-    }
-    return render(request, 'music_share/home.html', context)
-
-@login_required
-def my_songs(request):
-    context = {
-        'songs' : MusicFile.objects.filter(owner = request.user)
-    }
-    return render(request, 'music_share/home.html', context)
-
-
-def about(request):
-    return render(request, 'music_share/about.html', {'title': 'About'})
-
 
 @login_required
 def upload(request):
@@ -54,6 +39,7 @@ def upload(request):
             music_file.title = audio.tag.title if audio.tag.title else "Unknown Title"
             music_file.artist = audio.tag.artist if audio.tag.artist else "Unknown Artist"
             music_file.album = audio.tag.album if audio.tag.album else "Unknown Album"
+            # music_file.image = audio.tag.images[0] if audio.tag.images else 'default.jpeg'
 
             # Save MusicFile object to database
             music_file.save()
@@ -67,37 +53,70 @@ def upload(request):
     return render(request, 'music_share/upload.html', {'form': form})
 
 
-
-from django.http import HttpResponse, Http404
-from django.shortcuts import get_object_or_404, redirect
-from django.views.generic import ListView, DetailView
-from django.conf import settings
-import os
-from .models import MusicFile
-
-
-class SongListView(ListView):
+class SongListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
     model = MusicFile
     template_name = 'music_share/song_list.html'
-    paginate_by = 10
+    paginate_by = 6
+
+    def test_func(self) -> bool | None:
+        # Check if the user is authenticated 
+        return self.request.user.is_authenticated 
+
+    def get_queryset(self):
+        # Filter the queryset to only include songs owned by the current user, songs which are public and songs which user have access
+        queryset = super().get_queryset()
+        return queryset.filter(Q(owner=self.request.user) | Q(visibility='public') | Q(allowed_emails__contains=[self.request.user.email]))
+    
 
 
-class SongDetailView(DetailView):
+class UserSongListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
     model = MusicFile
-    template_name = 'music_share/song_detail.html'
+    template_name = 'music_share/song_list.html'
+    paginate_by = 6
+
+    def test_func(self) -> bool | None:
+        # Check if the user is authenticated and the owner of the songs
+        return self.request.user.is_authenticated and MusicFile.objects.filter(owner=self.request.user).exists()
+
+    def get_queryset(self):
+        # Filter the queryset to only include songs owned by the current user
+        queryset = super().get_queryset()
+        return queryset.filter(owner=self.request.user)
 
 
-def play_song(request, song_id):
-    song = get_object_or_404(MusicFile, id=song_id)
-    file_path = song.file.path
-    if os.path.exists(file_path):
-        with open(file_path, 'rb') as fh:
-            response = HttpResponse(fh.read(), content_type="audio/mpeg")
-            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
-            return response
-    raise Http404
+class SongPlayView(DetailView):
+    model = MusicFile
+    template_name = 'music_share/play_song.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['audio_file_url'] = self.object.file.url
+        return context
+    
+    def get(self, request, song_id):
+        song = get_object_or_404(MusicFile, id=song_id)
+        file_path = song.file.path
+        if os.path.exists(file_path):
+            context = {
+                'song': song
+            }
+            return render(request, 'music_share/play_song.html', context)
+        raise Http404
 
 
+# @login_required
+# def play_song(request, song_id):
+#     song = get_object_or_404(MusicFile, id=song_id)
+#     file_path = song.file.path
+#     if os.path.exists(file_path):
+#         with open(file_path, 'rb') as fh:
+#             response = HttpResponse(fh.read(), content_type="audio/mpeg")
+#             response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+#             return response
+#     raise Http404
+
+
+@login_required
 def download_song(request, song_id):
     song = get_object_or_404(MusicFile, id=song_id)
     file_path = song.file.path
